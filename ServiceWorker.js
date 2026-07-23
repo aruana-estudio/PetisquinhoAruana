871eb4def3bf4412f4b5dd9e67d26cc183460c8a
QuatiConde <aruanaestudio@gmail.com>
2026-07-23 10:09:35 -0300

    ajuste na colisão

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "AruanaEstudio-Petisquinho-1.0.9";
 const contentToCache = [
     "Build/9153f9efe3d6220f80d5dec24b2290ed.loader.js",
     "Build/185f3be041cd8cfbd9c10e3839d5e7c8.framework.js",
-    "Build/c73f4a8cd53494a4177def636d1086d9.data",
+    "Build/eb2f06b4ec5b3a6624ed91e3f8c6a077.data",
     "Build/be4101495df9f7010179ed2380bd3584.wasm",
     "TemplateData/style.css"
 
