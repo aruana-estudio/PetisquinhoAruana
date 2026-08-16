f0e6540d801d3f1d99a1da967b42e16d6f042e72
QuatiConde <aruanaestudio@gmail.com>
2026-08-16 11:43:28 -0300

    ataliza de bugs

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "AruanaEstudio-Petisquinho-1.0.9";
 const contentToCache = [
-    "Build/3cc620dbec6fa64a2c35d236b90e75b6.loader.js",
+    "Build/8a8a38c0dc815eedf25cb1fe5df89304.loader.js",
     "Build/185f3be041cd8cfbd9c10e3839d5e7c8.framework.js",
-    "Build/f608fb3b914f395422383bf459184905.data",
-    "Build/cc02872c026aad56aca16b52fbdec1ee.wasm",
+    "Build/6be66edd1e3ea7526d2e9c38f1423a1e.data",
+    "Build/9064292ed8b8d06be1aaf782f3a5dda2.wasm",
     "TemplateData/style.css"
 
 ];
